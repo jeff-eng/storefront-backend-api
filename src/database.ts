ce9d366105bf07afba5ab13f97e71b6629cd3f88
jeff-eng <me@jeffeng.com>
2023-02-26 16:33:02 -0800

    database.ts (db connection file) is more DRY.

--- src/database.ts
+++ src/database.ts
@@ -9,13 +9,15 @@ const {
     POSTGRES_HOST,
     POSTGRES_USER,
     POSTGRES_PASSWORD,
-    POSTGRES_DB
+    POSTGRES_DB,
+    POSTGRES_TEST_DB,
+    ENV
 } = process.env;
 
-// Create Postgres database connection for local development
-const client = new Pool({
+// Create Postgres database connection
+const client: Pool = new Pool({
     host: POSTGRES_HOST,
-    database: POSTGRES_DB,
+    database: ENV === 'test' ? POSTGRES_TEST_DB : POSTGRES_DB,
     user: POSTGRES_USER,
     password: POSTGRES_PASSWORD
 });
